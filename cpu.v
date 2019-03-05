0d14f389bb74492ae8569e1a21370cf566dd5f5d
Andrew Wei <andrewwei1999@gmail.com>
2019-03-04 17:46:52 -0800

    check if it still works

--- cpu.v
+++ cpu.v
@@ -27,13 +27,6 @@ module cpu(
   reg [data_addr_width - 1:0] data_addr; //use instead of the two above
   reg data_mem_we;
 
-  // Synchronous memories
-  always @(posedge clk) begin
-    if (data_mem_we)
-        data_mem[data_addr] <= data_mem_wd;
-    data_mem_rd <= data_mem[data_addr];
-  end
-
   localparam code_width = 32;
   localparam code_width_l2b = $clog2(code_width / 8);
   localparam code_words = 16;
@@ -218,6 +211,15 @@ function automatic [3:0] inst_opcode;
     inst_opcode = inst[24:21];
 endfunction
 
+
+
+
+
+
+/////////////////////////////////
+// FETCH
+
+
 //  "Fetch" from code memory into instruction bits
   reg [31:0] inst;
   always @(*) begin
@@ -235,6 +237,18 @@ endfunction
     else
       operand2 = inst_data_proc_imm(inst);
   end
+
+
+
+
+
+
+
+
+
+/////////////////////////////////
+// DECODE
+
 
   // "Decode" what gets read and written
   always @(*) begin
@@ -288,6 +302,18 @@ endfunction
     else
       branch_target = pc + 8 + inst_branch_imm(inst);
   end
+
+
+
+
+
+
+
+
+/////////////////////////////////
+// EXECUTE
+
+
 
   // "Execute" the instruction
   reg [32:0] alu_result;
@@ -394,6 +420,32 @@ endfunction
           rf_wd = alu_result[31:0];
       end
   end
+
+
+
+
+
+
+/////////////////////////////////
+// Memory
+
+  // Synchronous memories
+  always @(posedge clk) begin
+    if (data_mem_we)
+        data_mem[data_addr] <= data_mem_wd;
+    data_mem_rd <= data_mem[data_addr];
+  end
+
+
+
+
+
+
+
+/////////////////////////////////
+// Write back register
+
+
 
   // "Write back" the instruction
   always @(posedge clk) begin
